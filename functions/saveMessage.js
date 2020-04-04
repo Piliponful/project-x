cd0bc987543047d1285229a8be5720a86e8ae9c4
piliponful <piliponful@gmail.com>
2020-04-04 22:51:25 +0300

    add npm run dev script, add nodemon, change return value of saveMessage

--- functions/saveMessage.js
+++ functions/saveMessage.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb'
 const getConnectedClient = async () => {
   const url = 'mongodb://piliponful:password123@localhost:27017/test'
 
-  const client = await MongoClient.connect(url)
+  const client = await (new MongoClient(url, { useUnifiedTopology: true })).connect()
 
   return client
 }
@@ -15,11 +15,11 @@ const saveMessage = async ({ content }) => {
 
   const messagesCollection = db.collection('messages')
 
-  await messagesCollection.insertOne({ content })
+  const result = await messagesCollection.insertOne({ content })
 
   await connectedClient.close()
 
-  return { success: true }
+  return { success: true, message: { content, id: result.insertedId } }
 }
 
 export default saveMessage
