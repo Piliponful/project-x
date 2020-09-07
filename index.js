dcb8c1fe8e974d7ac6b8a78ce98d983de0df00f3
piliponful <piliponful@gmail.com>
2020-09-07 23:27:47 +0300

    implement srpc func for fetching groups

--- index.js
+++ index.js
@@ -6,19 +6,22 @@ import getMessages from './functions/srpc/getMessages'
 import createUser from './functions/srpc/createUser'
 import verifyUser from './functions/srpc/verifyUser'
 import createGroup from './functions/srpc/createGroup'
+import getGroups from './functions/srpc/getGroups'
 
 import createUserValidation from './functions/paramsValidation/createUserValidation'
 import getMessagesValidation from './functions/paramsValidation/getMessagesValidation'
 import saveMessageValidation from './functions/paramsValidation/saveMessageValidation'
 import verifyUserValidation from './functions/paramsValidation/verifyUserValidation'
 import createGroupValidation from './functions/paramsValidation/createGroupValidation'
+import getGroupsValidation from './functions/paramsValidation/getGroupsValidation'
 
 const functions = {
   createUser,
   getMessages,
   saveMessage,
   verifyUser,
-  createGroup
+  createGroup,
+  getGroups
 }
 
 const paramsValidationFunctions = {
@@ -26,7 +29,8 @@ const paramsValidationFunctions = {
   getMessages: getMessagesValidation,
   saveMessage: saveMessageValidation,
   verifyUser: verifyUserValidation,
-  createGroup: createGroupValidation
+  createGroup: createGroupValidation,
+  getGroups: getGroupsValidation
 }
 
 const port = 8080
