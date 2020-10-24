7da08e116eef5f0cc901222bf96ebdc173859952
piliponful <piliponful@gmail.com>
2020-10-24 14:09:15 +0300

    implement logic that gets userIds from group and filters messages by them

--- constants/jwtSecret.js
+++ constants/jwtSecret.js
@@ -1,1 +1,2 @@
 export const secret = 'super_duper_secret_123'
+export const compositionType = ['union', 'intersection', 'difference']
--- functions/srpc/getMessages.js
+++ functions/srpc/getMessages.js
@@ -1,19 +1,68 @@
-import { omit } from 'lodash'
+import { omit, difference, union, intersection } from 'lodash'
+import { ObjectID } from 'mongodb'
 
 import getConnectedClient from '../getConnectedClient'
 
-const getMessages = async () => {
+const compositionFunctions = {
+  difference,
+  union,
+  intersection
+}
+
+const unravelGroup = async ({ messageId, content, groupIdLeft, groupIdRight, compositionType }, db) => {
+  if (compositionType) {
+    const groupLeft = await db.groupsCollection.findOne({ groupId: groupIdLeft })
+    const groupRight = await db.groupsCollection.findOne({ groupId: groupIdRight })
+
+    return { groupLeft: unravelGroup(groupLeft), groupRight: unravelGroup(groupRight), compositionType }
+  }
+
+  const messages = (await db.messagesCollection.find({ parentMessageId: messageId, content }).toArray())
+
+  const userIds = messages.map(i => i.userId)
+
+  return userIds
+}
+
+const getUserIdsFromGroupTree = ({ groupLeft, groupRight, compositionType }) =>
+  compositionFunctions[compositionType](
+    Array.isArray(groupLeft) ? groupLeft : getUserIdsFromGroupTree(groupLeft),
+    Array.isArray(groupRight) ? groupRight : getUserIdsFromGroupTree(groupRight)
+  )
+
+const getMessagesByGroup = async (group, db) => {
+  const groupTree = await unravelGroup(group, db)
+
+  const userIds = Array.isArray(groupTree) ? groupTree : getUserIdsFromGroupTree(groupTree)
+  const query = { userId: { $in: userIds } }
+  const messages = (await db.messagesCollection.find(query).toArray()).map(i => ({ ...omit(i, '_id'), id: i._id }))
+
+  return messages
+}
+
+const getMessages = async ({ groupId } = {}) => {
   const connectedClient = await getConnectedClient()
 
   const db = connectedClient.db()
 
   const messagesCollection = db.collection('messages')
+  const groupsCollection = db.collection('groups')
 
-  const messages = (await messagesCollection.find().toArray()).map(i => ({ ...omit(i, '_id'), id: i._id }))
+  const group = groupId ? await groupsCollection.findOne({ _id: new ObjectID(groupId) }) : null
 
-  await connectedClient.close()
+  if (!group) {
+    const messages = (await messagesCollection.find().toArray()).map(i => ({ ...omit(i, '_id'), id: i._id }))
 
-  return { success: true, messages }
+    await connectedClient.close()
+
+    return { success: true, messages }
+  } else {
+    const messages = await getMessagesByGroup(group, { messagesCollection, groupsCollection })
+
+    await connectedClient.close()
+
+    return { success: true, messages }
+  }
 }
 
 export default getMessages
