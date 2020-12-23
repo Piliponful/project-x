c6f381b4090321f9ec4c2eb76ffe8551af813e5e
piliponful <piliponful@gmail.com>
2020-12-23 17:08:48 +0200

    unselect previous group before selecting new one

--- functions/srpc/setSelectedGroup.js
+++ functions/srpc/setSelectedGroup.js
@@ -27,6 +27,8 @@ const setSelectedGroup = async ({ jwt, groupId }) => {
     return { success: false }
   }
 
+  await groupsCollection.findOneAndUpdate({ selected: true }, { $unset: { selected: true } })
+
   await groupsCollection.updateOne({ _id: new ObjectID(groupId) }, { $set: { selected: true } })
 
   return { success: true }
