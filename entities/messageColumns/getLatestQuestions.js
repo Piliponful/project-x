2fddf8fc41cf4cfd89580ab27c6cdc02319085df
piliponful <piliponful@gmail.com>
2021-02-18 16:53:46 +0200

    implement questions rows logic #38

---
+++ entities/messageColumns/getLatestQuestions.js
@@ -0,0 +1,55 @@
+import { ObjectID } from 'mongodb'
+import { decode as decodeJwt } from 'jwt-simple'
+import { omit, orderBy } from 'lodash'
+
+import getConnectedClient from '../../functions/getConnectedClient'
+import { getMessagesByGroup } from '../group'
+
+import { secret } from '../../constants/jwtSecret'
+
+const getLatestQuestions = async ({ jwt }) => {
+  const connectedClient = await getConnectedClient()
+
+  const db = connectedClient.db()
+
+  const messagesCollection = db.collection('messages')
+  const groupsCollection = db.collection('groups')
+  const usersCollection = db.collection('users')
+
+  const { userId } = decodeJwt(jwt, secret)
+
+  const user = await usersCollection.findOne({ _id: new ObjectID(userId), verificationCode: { $exists: false } })
+
+  if (!user) {
+    return { success: false }
+  }
+
+  const group = await groupsCollection.findOne({ selected: true })
+
+  const getActionsByQuery = query => (group
+    ? getMessagesByGroup({ messagesCollection, groupsCollection }, group, query)
+    : messagesCollection.find(query))
+
+  const omitId = arr => arr.map(i => ({ ...omit(i, '_id'), id: i._id.toString() }))
+
+  const answers = omitId(await getActionsByQuery({ parentMessageId: { $ne: null } }).toArray())
+  const questions = omitId(await getActionsByQuery({ parentMessageId: { $eq: null } }).toArray())
+
+  const questionsWithAnswers = questions
+    .map(m => ({
+      ...m,
+      answersCount: {
+        yes: answers.filter(a => m.id === a.parentMessageId && a.content.toLowerCase() === 'yes').length,
+        no: answers.filter(a => m.id === a.parentMessageId && a.content.toLowerCase() === 'no').length
+      },
+      currentUserAnswer: (answers.find(a => a.parentMessageId === m.id && a.userId === userId) || {}).content
+    }))
+
+  const orderedQuestions = orderBy(questionsWithAnswers, 'createdAt', 'desc')
+
+  await connectedClient.close()
+
+  return { success: true, messages: orderedQuestions }
+}
+
+export default getLatestQuestions
--- functions/srpc/getMessages.js
+++ functions/srpc/getMessages.js
@@ -1,61 +1,19 @@
-import { ObjectID } from 'mongodb'
-import { decode as decodeJwt } from 'jwt-simple'
-import { omit } from 'lodash'
+import getMostAnsweredQuestions from '../../entities/messageColumns/getMostAnsweredQuestions'
+import getLatestQuestions from '../../entities/messageColumns/getLatestQuestions'
+import getControversialQuestions from '../../entities/messageColumns/getControversialQuestions'
+import getUnanimousQuestions from '../../entities/messageColumns/getUnanimousQuestions'
 
-import getConnectedClient from '../getConnectedClient'
-import { unravelGroup, getUserIdsFromGroupTree } from '../../entities/group'
-
-import { secret } from '../../constants/jwtSecret'
-
-const getMessagesByGroup = async (group, db) => {
-  const groupTree = await unravelGroup(group, db)
-
-  const userIds = Array.isArray(groupTree) ? groupTree : getUserIdsFromGroupTree(groupTree)
-
-  return db.messagesCollection.find({ userId: { $in: userIds } })
+const messageColumnToFunc = {
+  mostAnswered: getMostAnsweredQuestions,
+  latest: getLatestQuestions,
+  controversial: getControversialQuestions,
+  unanimous: getUnanimousQuestions
 }
 
-const getMessages = async ({ jwt }) => {
-  const connectedClient = await getConnectedClient()
+const getMessages = async ({ jwt, messageColumn = 'mostAnswered' }) => {
+  const result = await messageColumnToFunc[messageColumn]({ jwt })
 
-  const db = connectedClient.db()
-
-  const messagesCollection = db.collection('messages')
-  const groupsCollection = db.collection('groups')
-  const usersCollection = db.collection('users')
-
-  const { userId } = decodeJwt(jwt, secret)
-
-  const user = await usersCollection.findOne({ _id: new ObjectID(userId), verificationCode: { $exists: false } })
-
-  if (!user) {
-    return { success: false }
-  }
-
-  const group = await groupsCollection.findOne({ selected: true })
-
-  const actions1 = await (group
-    ? getMessagesByGroup(group, { messagesCollection, groupsCollection })
-    : messagesCollection.find())
-
-  const actions = (await actions1.toArray()).map(i => ({ ...omit(i, '_id'), id: i._id.toString() }))
-
-  const answers = actions.filter(i => i.parentMessageId)
-  const questions = actions.filter(i => !i.parentMessageId)
-
-  const questionsWithAnswers = questions
-    .map(m => ({
-      ...m,
-      answersCount: {
-        yes: answers.filter(a => m.id === a.parentMessageId && a.content.toLowerCase() === 'yes').length,
-        no: answers.filter(a => m.id === a.parentMessageId && a.content.toLowerCase() === 'no').length
-      },
-      currentUserAnswer: (answers.find(a => a.parentMessageId === m.id && a.userId === userId) || {}).content
-    }))
-
-  await connectedClient.close()
-
-  return { success: true, messages: questionsWithAnswers }
+  return { ...result, messageColumn }
 }
 
 export default getMessages
