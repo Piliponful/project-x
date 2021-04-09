3c924206378cb8970778ff42a4faf4f4cf25501f
piliponful <piliponful@gmail.com>
2021-04-09 14:19:51 +0300

    change sign up flow, change phone validation, check for username and phone uniqueness

--- functions/paramsValidation/createUserValidation.js
+++ functions/paramsValidation/createUserValidation.js
@@ -1,5 +1,7 @@
+const phone = require('phone')
+
 const createUserValidation = ({ username, phoneNumber, password }) => {
-  return username && phoneNumber && password
+  return username && phone(phoneNumber).length !== 0 && password
 }
 
 module.exports = createUserValidation
