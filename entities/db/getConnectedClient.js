833e122b30e69f475ee78588e097e1310a6beb8b
piliponful <piliponful@gmail.com>
2021-04-10 13:42:52 +0300

    deploy backend to digitalocean #52

--- entities/db/getConnectedClient.js
+++ entities/db/getConnectedClient.js
@@ -2,6 +2,7 @@ import { MongoClient } from 'mongodb'
 
 const getConnectedClient = async () => {
   const { DB_HOST, DB_USER, DB_USER_PASSWORD, DB_NAME, DB_PORT } = process.env
+  console.log(`mongodb://${DB_USER}:${DB_USER_PASSWORD}@${DB_HOST}${DB_PORT ? `:${DB_PORT}` : ''}/${DB_NAME}`)
 
   const url = `mongodb://${DB_USER}:${DB_USER_PASSWORD}@${DB_HOST}${DB_PORT ? `:${DB_PORT}` : ''}/${DB_NAME}`
 
